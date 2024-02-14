8764b7203266a8aaa3c5d0bf8bb8c8774ebd4082
Ryan Samuels <131621582+returnofblank@users.noreply.github.com>
2024-02-14 03:54:03 -0500

    save

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My various system flakes";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.3.0";
+      # Optional but recommended to limit the size of your system closure.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {nixpkgs, ...}: {
+    nixosConfigurations = {
+      hal-9000 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          
+        ];
+      };
+    };
+  };
+}
