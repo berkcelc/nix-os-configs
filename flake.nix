cb1f47250c9c4d71cd23c30dbb92640acbb38062
Ryan Samuels <131621582+returnofblank@users.noreply.github.com>
2024-02-14 04:04:07 -0500

    change

--- flake.nix
+++ flake.nix
@@ -1,24 +1,34 @@
 {
-  description = "My various system flakes";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    description = "My various system flakes";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.3.0";
-      # Optional but recommended to limit the size of your system closure.
-      inputs.nixpkgs.follows = "nixpkgs";
+        lanzaboote = {
+        url = "github:nix-community/lanzaboote/v0.3.0";
+        # Optional but recommended to limit the size of your system closure.
+        inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
 
-  outputs = {nixpkgs, ...}: {
-    nixosConfigurations = {
-      hal-9000 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          
-        ];
-      };
+    outputs = {self, nixpkgs, lanzaboote, ...}: {
+        nixosConfigurations = {
+            hal-9000 = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [
+                    ./configuration.nix
+                
+                    # Lanzaboote (Secure Boot)
+                    lanzaboote.nixosModules.lanzaboote
+                    ({ pkgs, lib, ... }: {
+                        boot.loader.systemd-boot.enable = lib.mkForce false;
+
+                        boot.lanzaboote = {
+                        enable = true;
+                        pkiBundle = "/etc/secureboot";
+                        };
+                    })
+                ];
+            };
+        };
     };
-  };
 }
