0cf38892e73fd47f1563fb078824094106dd63ac
Ryan Samuels <131621582+returnofblank@users.noreply.github.com>
2024-02-15 22:53:01 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -31,6 +31,20 @@
                             pkiBundle = "/etc/secureboot";
                         };
                     })
+                    ({ config, pkgs, inputs, ... }:
+
+                    let nixPath = "/etc/nixPath";
+
+                    in
+                    {
+                      systemd.tmpfiles.rules = [
+                        "L+ ${nixPath} - - - - ${pkgs.path}"
+                      ];
+
+                      nix = {
+                        nixPath = [ "nixpkgs=${nixPath}" ];
+                      };
+                    })
                 ];
             };
         };
